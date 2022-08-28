1aac8377b016f417e13eddcfae19134d3edbeebd
pytoncraft2 <pythoncraft2@gmail.com>
2022-08-28 16:26:17 +0200

    [+] scrollfactor et répétition image

--- src/scenes/Eau.js
+++ src/scenes/Eau.js
@@ -11,7 +11,7 @@ const createAligned = (scene, totalWidth, texture, scrollFactor) => {
 
 		const m = scene.add.image(x, scene.scale.height, texture)
 			.setScrollFactor(scrollFactor)
-			// .setOrigin(0, 1)
+			.setOrigin(0, 1)
 		x += m.width
 	}
 }
@@ -23,14 +23,6 @@ class Eau extends Phaser.Scene {
 	constructor() {
 		super("Eau");
 
-		/** @type {Phaser.GameObjects.Image} */
-		this.mer_loin;
-		/** @type {Phaser.GameObjects.Image} */
-		this.algue_milieu;
-		/** @type {Phaser.GameObjects.Image} */
-		this.algue_proche;
-
-
 		/* START-USER-CTR-CODE */
 		// Write your code here.
 		/* END-USER-CTR-CODE */
@@ -39,26 +31,12 @@ class Eau extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
-		// mer_loin
-		const mer_loin = this.add.image(903, 193, "sea_background");
-
-		// algue_milieu
-		const algue_milieu = this.add.image(915, 188, "farground");
-
-		// algue_proche
-		const algue_proche = this.add.image(907, 287, "mid_background");
-
 		// rectangle_1
-		const rectangle_1 = this.add.rectangle(-353, -13, 128, 128);
+		const rectangle_1 = this.add.rectangle(-295, 118, 128, 128);
 		rectangle_1.isFilled = true;
-
-		this.mer_loin = mer_loin;
-		this.algue_milieu = algue_milieu;
-		this.algue_proche = algue_proche;
 
 		this.events.emit("scene-awake");
 	}
-
 
 	/* START-USER-CODE */
 
@@ -71,23 +49,13 @@ class Eau extends Phaser.Scene {
 		const width = this.scale.width
 		const height = this.scale.height
 
-		const totalWidth = width * 10
+		const totalWidth = width * 20
 
 
-		this.cameras.main.setBounds(0, 0, width * 3, height)
+		this.cameras.main.setBounds(0, 0, width * 20, height)
 		this.cursors = this.input.keyboard.createCursorKeys();
 
-		// this.mer_loin
-		// 	.setScrollFactor(0)
-
-		// this.algue_milieu
-		// 	.setScrollFactor(0.25)
-
-		// this.algue_milieu
-		// 	.setScrollFactor(0.5)
-
-
-		createAligned(this, totalWidth, "sea_background", 0)
+		createAligned(this, totalWidth, "sea_background", 0.1)
 		createAligned(this, totalWidth, "farground", 0.25)
 		createAligned(this, totalWidth, "mid_background", 0.5)
 
@@ -95,7 +63,7 @@ class Eau extends Phaser.Scene {
 
 	update() {
 		const cam = this.cameras.main
-		const speed = 9;
+		const speed = 20;
 
 		if (this.cursors.left.isDown) {
 			cam.scrollX -= speed
