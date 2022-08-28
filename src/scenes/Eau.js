8ab9a08cb0595e2c5340b786a6ec08595b79194f
pytoncraft2 <pythoncraft2@gmail.com>
2022-08-28 16:02:56 +0200

    [~] parallax ok

--- src/scenes/Eau.js
+++ src/scenes/Eau.js
@@ -8,6 +8,14 @@ class Eau extends Phaser.Scene {
 	constructor() {
 		super("Eau");
 
+		/** @type {Phaser.GameObjects.Image} */
+		this.mer_loin;
+		/** @type {Phaser.GameObjects.Image} */
+		this.algue_milieu;
+		/** @type {Phaser.GameObjects.Image} */
+		this.algue_proche;
+
+
 		/* START-USER-CTR-CODE */
 		// Write your code here.
 		/* END-USER-CTR-CODE */
@@ -16,17 +24,26 @@ class Eau extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
-		// sea_background
-		this.add.image(903, 193, "sea_background");
+		// mer_loin
+		const mer_loin = this.add.image(903, 193, "sea_background");
 
-		// farground
-		this.add.image(915, 188, "farground");
+		// algue_milieu
+		const algue_milieu = this.add.image(915, 188, "farground");
 
-		// mid_background
-		this.add.image(907, 287, "mid_background");
+		// algue_proche
+		const algue_proche = this.add.image(907, 287, "mid_background");
+
+		// rectangle_1
+		const rectangle_1 = this.add.rectangle(-528, 155, 128, 128);
+		rectangle_1.isFilled = true;
+
+		this.mer_loin = mer_loin;
+		this.algue_milieu = algue_milieu;
+		this.algue_proche = algue_proche;
 
 		this.events.emit("scene-awake");
 	}
+
 
 	/* START-USER-CODE */
 
@@ -38,7 +55,19 @@ class Eau extends Phaser.Scene {
 
 		const width = this.scale.width
 		const height = this.scale.height
+
+
+		this.cameras.main.setBounds(0, 0, width * 3, height)
 		this.cursors = this.input.keyboard.createCursorKeys();
+
+		this.mer_loin
+			.setScrollFactor(0)
+		
+		this.algue_milieu
+			.setScrollFactor(0.25)
+		
+		this.algue_milieu
+			.setScrollFactor(0.5)
 
 	}
 
